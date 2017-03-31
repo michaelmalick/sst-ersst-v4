2ef5c349854466352eb3de1b510faae1ab020474
Michael Malick <malickmj@gmail.com>
2017-03-31 12:04:30 -0700

    Update SASAP SST export

--- export.R
+++ export.R
@@ -3,15 +3,18 @@
 
 
 ## SASAP N. Pacific SST ------------------------------------
-sasap.raw <- sst_load(years = 1950:2016,
-                      months = 1:12,
+sasap.raw <- sst_load(years    = 1950:2016,
+                      months   = 1:12,
                       read.dir = "./data/rawdata/")
 
 sasap.raw.np <- sst_subset_space(sasap.raw,
-                                  lat.min = 20,
-                                  lat.max = 80,
-                                  lon.min = 130,
-                                  lon.max = 260)
+                                 lat.min = 20,
+                                 lat.max = 80,
+                                 lon.min = 130,
+                                 lon.max = 260)
+
+sasap.anom.np <- sst_anomaly(sasap.raw.np,
+                             ref.years = 1950:2016)
 
 sasap.raw.np.df <- sst_dataframe(sasap.raw.np)
 head(sasap.raw.np.df)
@@ -21,9 +24,20 @@ levelplot(sst ~ lon * lat, data = sasap.raw.np.df,
           contour = TRUE,
           ylab = "Latitude", xlab = "Longitude")
 
+sasap.anom.np.df <- sst_dataframe(sasap.anom.np)
+head(sasap.anom.np.df)
+tail(sasap.anom.np.df)
+levelplot(sst ~ lon * lat, data = sasap.anom.np.df,
+          subset = month == 1 & year == 2015,
+          # subset = month == 1 & year == 1950,
+          at = seq(-4.5, 4.5, 0.5),
+          contour = TRUE,
+          labels = TRUE,
+          ylab = "Latitude", xlab = "Longitude")
+
 sst_write_csv(data = sasap.raw.np.df,
-              file = "./data/export/sasap-ersstv4-2017-03-29.csv",
-              date = "2017-03-29",
+              file = "./data/export/sasap-ersstv4-2017-03-31.csv",
+              date = "2017-03-31",
               contact = "Michael Malick <malickmj@gmail.com>",
               data.type = "SST values",
               years = "1950-2016",
